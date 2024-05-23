0738217b86fb107336e413a93ee13ca5368a2541
sebastiandevus <sebastiandevus@gmail.com>
2024-05-22 21:34:53 -0300

    Cria script que adiciona navbar automaticamente

---
+++ carregaNavbar.js
@@ -0,0 +1,18 @@
+document.write(`
+<nav class="navbar navbar-expand-sm bg-black">
+
+        <ul class="navbar-nav">
+            <li class="nav-item ps-3">
+                <a  class="nav-link" href="index.html">Inserir</a>
+            </li>
+            <li class="nav-item ps-3">
+                <a  class="nav-link" href="">Editar</a>
+            </li>
+            <li class="nav-item ps-3">
+                <a  class="nav-link" href="formularioPM.html">Formulario anterior</a>
+            </li>
+        </ul>
+
+
+</nav>
+`)
