114795192cb2bb844808ba3d4540516ef635b9cb
SebastianDevus <86898507+SebastianDevus@users.noreply.github.com>
2024-06-04 20:56:29 -0300

    Alteração no cadastro
    armazena cadastros no localStorage

--- modulos/fazCadastro.js
+++ modulos/fazCadastro.js
@@ -1,8 +1,5 @@
-// Antes do DOM carregar
-import { Jogador } from "./exports.js";
-var vet = [];
+import { Jogador } from "./exports.js"
 
-// Dps do DOM carregar
 addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("form");
     const inputNome = document.getElementById("inputNome");
@@ -15,20 +12,22 @@ addEventListener("DOMContentLoaded", () => {
     const checkRotativos = document.getElementById("checkRotativos");
     const inputRota = document.getElementById("inputRota");
     const inputRank = document.getElementById("inputRank");
+    let vet = JSON.parse(localStorage.getItem("")) || [];
 
     form.onsubmit = function (e) {
         e.preventDefault();
         let escolheuRift = checkSummoner.checked
         let escolheuAram = checkAram.checked
         let escolheuRotativos = checkRotativos.checked
-
-        if (escolheuRift || escolheuAram || escolheuRotativos) {
+        
+        if (escolheuRift || escolheuAram || escolheuRotativos) {  
             let jogador = new Jogador(inputNome.value, inputComeco.value, inputNivel.value, inputMain.value, inputOdeia.value, 
-                escreveModos(escolheuRift, escolheuAram, escolheuRotativos, inputRota.value, inputRank.value))
-            vet.push(jogador)
-        } else {
-            alert("Por favor, escolha pelo menos um modo de jogo");
-        }
+                escreveModos(escolheuRift, escolheuAram, escolheuRotativos), inputRota.value, inputRank.value)
+                vet.push(jogador)
+                localStorage.setItem("jogador",JSON.stringify(vet))
+            } else {
+                alert("Por favor, escolha pelo menos um modo de jogo");
+            }
     }
 })
 
