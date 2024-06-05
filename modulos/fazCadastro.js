371c5920680d28cad379d0571f04a591c073a842
SebastianDevus <86898507+SebastianDevus@users.noreply.github.com>
2024-06-04 21:08:14 -0300

    Altera posição da função escreveModos

--- modulos/fazCadastro.js
+++ modulos/fazCadastro.js
@@ -1,36 +1,5 @@
 import { Jogador } from "./exports.js"
 
-addEventListener("DOMContentLoaded", () => {
-    const form = document.getElementById("form");
-    const inputNome = document.getElementById("inputNome");
-    const inputComeco = document.getElementById("inputComeco");
-    const inputNivel = document.getElementById("inputNivel");
-    const inputMain = document.getElementById("inputMain");
-    const inputOdeia = document.getElementById("inputOdeia");
-    const checkSummoner = document.getElementById("checkSummoner");
-    const checkAram = document.getElementById("checkAram");
-    const checkRotativos = document.getElementById("checkRotativos");
-    const inputRota = document.getElementById("inputRota");
-    const inputRank = document.getElementById("inputRank");
-    let vet = JSON.parse(localStorage.getItem("")) || [];
-
-    form.onsubmit = function (e) {
-        e.preventDefault();
-        let escolheuRift = checkSummoner.checked
-        let escolheuAram = checkAram.checked
-        let escolheuRotativos = checkRotativos.checked
-        
-        if (escolheuRift || escolheuAram || escolheuRotativos) {  
-            let jogador = new Jogador(inputNome.value, inputComeco.value, inputNivel.value, inputMain.value, inputOdeia.value, 
-                escreveModos(escolheuRift, escolheuAram, escolheuRotativos), inputRota.value, inputRank.value)
-                vet.push(jogador)
-                localStorage.setItem("jogador",JSON.stringify(vet))
-            } else {
-                alert("Por favor, escolha pelo menos um modo de jogo");
-            }
-    }
-})
-
 function escreveModos(summonersRift, aram, modosRotativos) {
     let primeiro = true
     let string;
@@ -58,3 +27,34 @@ function escreveModos(summonersRift, aram, modosRotativos) {
 
     return string
 }
+
+addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("form");
+    const inputNome = document.getElementById("inputNome");
+    const inputComeco = document.getElementById("inputComeco");
+    const inputNivel = document.getElementById("inputNivel");
+    const inputMain = document.getElementById("inputMain");
+    const inputOdeia = document.getElementById("inputOdeia");
+    const checkSummoner = document.getElementById("checkSummoner");
+    const checkAram = document.getElementById("checkAram");
+    const checkRotativos = document.getElementById("checkRotativos");
+    const inputRota = document.getElementById("inputRota");
+    const inputRank = document.getElementById("inputRank");
+    let vet = JSON.parse(localStorage.getItem("")) || [];
+
+    form.onsubmit = function (e) {
+        e.preventDefault();
+        let escolheuRift = checkSummoner.checked
+        let escolheuAram = checkAram.checked
+        let escolheuRotativos = checkRotativos.checked
+
+        if (escolheuRift || escolheuAram || escolheuRotativos) {
+            let jogador = new Jogador(inputNome.value, inputComeco.value, inputNivel.value, inputMain.value, inputOdeia.value,
+                escreveModos(escolheuRift, escolheuAram, escolheuRotativos), inputRota.value, inputRank.value)
+            vet.push(jogador)
+            localStorage.setItem("jogador", JSON.stringify(vet))
+        } else {
+            alert("Por favor, escolha pelo menos um modo de jogo");
+        }
+    }
+})
